cd974b42c3948955a0d3a4b6feca3a7dff009323
quank123wip <quank123wip@outlook.com>
2024-07-27 22:43:47 +0800

    chore🛠️: added basic layout for problemset

---
+++ apps/web/pages/problemset.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container flex-1 flex-row mt-8">
+
+        <div class="flex-grow"></div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
