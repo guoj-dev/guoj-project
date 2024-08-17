454e0560f56e297e995f76ffb6832fc6e7a2f8d5
quank123wip <quank123wip@outlook.com>
2024-08-17 21:33:18 +0800

    chore🛠️: user layout improved

--- apps/web/pages/user/me.vue
+++ apps/web/pages/user/me.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="container flex flex-col flex-grow h-full">
-        <div class="flex-grow h-full w-full border-x">
-            <div class="h-16rem border-b overflow-hidden relative">
+        <div class="flex flex-col flex-grow h-full w-full">
+            <div class="h-16rem border-b border-x shadow-sm overflow-hidden relative">
+                <div class="user-heading flex items-center absolute bottom-1rem left-3rem">
+                    <Avatar size="base" class="border-white border-2px"></Avatar>
+                    <a class="ml-4 text-shadow-md color-white">Username</a>
+                </div>
+            </div>
+            <div class="flex flex-grow mt-8">
+                <div class="flex-grow rounded-t-3xl shadow-xl border"></div>
+                <div>
+                    
+                </div>
             </div>
         </div>
     </div>
