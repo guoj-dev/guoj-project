83267225ac3f6845f1e52a2a767b26fd73e1d35f
quank123wip <quank123wip@outlook.com>
2024-08-20 18:20:34 +0800

    feat🛠️: tag api

---
+++ apps/server/src/routes/tag/index.ts
@@ -0,0 +1,20 @@
+import { db } from "@/db";
+import { tags } from "@/db/schema";
+import { NotFoundException } from "@/plugins/error/exceptions";
+import { useElysia } from "@/setup";
+import { eq } from "drizzle-orm";
+import { t } from "elysia";
+
+const route = useElysia({ prefix: "tag" })
+    .get("/", async ({ logger }) => {
+        const res = await db.query.tags.findMany();
+        return res;
+    })
+    .get("/:id", async ({ params: { id }, logger }) => {
+        const res = await db.query.tags.findFirst({
+            where: eq(tags.id, id),
+        });
+        return res;
+    });
+
+export default route;
