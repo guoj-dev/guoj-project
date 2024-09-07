bce009d55c4a62d584e566f2ab1e99daf8a36b4e
quank123wip <quank123wip@outlook.com>
2024-09-07 23:19:21 +0800

    feat🛠️: user home page layout

--- apps/web/pages/user/me.vue
+++ apps/web/pages/user/me.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="container flex flex-col flex-grow h-full">
-        
+        <div class="grid grid-cols-8 flex-grow pt-16">
+            <div class="col-span-2">
+                <div>
+                    <div class="aspect-square w-full bg-gray-200 rounded-full"></div>
+                    <div class="mt-4 mx-4">
+                        <p class="font-bold text-xl">Username</p>
+                        <p class="font-extralight text-lg">@Username</p>
+                        <p class="mt-2 font-light text-md">Your Bio Here.</p>
+                        <button class="mt-1 py-1 w-full rounded border bg-gray-50">Follow</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-span-5">
+                <div class="border w-full h-full ml-8 rounded-lg">
+
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
