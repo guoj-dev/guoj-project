827b1148d6da62434d65a00b7c4c13cd69f1f3e4
quank123wip <quank123wip@outlook.com>
2024-09-23 21:23:04 +0800

    chore🛠️: update database schema

--- apps/server/src/db/schema/judger.schema.ts
+++ apps/server/src/db/schema/judger.schema.ts
@@ -1,10 +1,38 @@
-import { real, json, pgEnum, pgTable, uuid, text } from "drizzle-orm/pg-core";
+import { real, json, pgEnum, pgTable, uuid, text, primaryKey } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { users } from "./user.schema";
-import { problems } from "./problemset.schema";
+import { problemsets } from "./problemset.schema";
+import { organizations } from "./organization.schema";
+
+export const judgerOwner = pgTable("judger_owner", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").references(() => users.id),
+    organizationId: uuid("organization_id").references(() => organizations.id),
+});
 
 export const judgers = pgTable("judgers", {
     id: uuid("id").primaryKey().defaultRandom(),
     name: text("name"),
-    
-})
+    token: text("token"),
+    owner: uuid("owner").references(() => judgerOwner.id, { onDelete: "cascade" }),
+    assignmentId: uuid("assignment_id").references(() => judgerAssignment.id, { onDelete: "cascade" }),
+});
+
+export const judgerAssignment = pgTable("judger_assignment", {
+    id: uuid("id").primaryKey().defaultRandom(),
+});
+
+export const assignmentToProblemset = pgTable(
+    "assignment_to_problemset",
+    {
+        assignmentId: uuid("assignment_id")
+            .notNull()
+            .references(() => users.id),
+        problemsetId: uuid("problemset_id")
+            .notNull()
+            .references(() => problemsets.id),
+    },
+    (t) => ({
+        pk: primaryKey({ columns: [t.assignmentId, t.problemsetId] }),
+    })
+);
--- apps/server/src/db/schema/organization.schema.ts
+++ apps/server/src/db/schema/organization.schema.ts
@@ -1,16 +1,9 @@
-import {
-    integer,
-    pgTable,
-    primaryKey,
-    uuid,
-    text,
-    pgEnum,
-} from "drizzle-orm/pg-core";
+import { integer, pgTable, primaryKey, uuid, text, pgEnum } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { users } from "./user.schema";
 import { problemsets } from "./problemset.schema";
 
-export const organizationRole = pgEnum("organizationRole", ["G", "U", "A", "R"])// Guest, User, Admin, Root
+export const organizationRole = pgEnum("organizationRole", ["G", "U", "A", "R"]); // Guest, User, Admin, Root
 
 export const organizations = pgTable("organization", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -26,30 +19,24 @@ export const organizationMember = pgTable(
             .references(() => users.id),
         organizationId: uuid("organization_id")
             .notNull()
-            .references(() => users.id),
-        organizationRole: organizationRole("organization_role")
+            .references(() => organizations.id),
+        organizationRole: organizationRole("organization_role"),
     },
     (t) => ({ pk: primaryKey({ columns: [t.userId, t.organizationId] }) })
 );
 
-export const organizationRelations = relations(
-    organizations,
-    ({ one, many }) => ({
-        organizationMember: many(organizationMember),
-        problemset: many(problemsets)
-    })
-);
+export const organizationRelations = relations(organizations, ({ one, many }) => ({
+    organizationMember: many(organizationMember),
+    problemset: many(problemsets),
+}));
 
-export const organizationMemberRelation = relations(
-    organizationMember,
-    ({ one }) => ({
-        user: one(users, {
-            fields: [organizationMember.userId],
-            references: [users.id],
-        }),
-        organization: one(organizations, {
-            fields: [organizationMember.organizationId],
-            references: [organizations.id],
-        }),
-    })
-);
+export const organizationMemberRelation = relations(organizationMember, ({ one }) => ({
+    user: one(users, {
+        fields: [organizationMember.userId],
+        references: [users.id],
+    }),
+    organization: one(organizations, {
+        fields: [organizationMember.organizationId],
+        references: [organizations.id],
+    }),
+}));
