fa267ca3e3d4550eab3d432401dafec68f6c8ddc
quank123wip <quank123wip@outlook.com>
2024-09-26 08:09:56 +0800

    chore🛠️: prepare to switch to eden treaty.

--- apps/server/src/client.ts
+++ apps/server/src/client.ts
@@ -1,8 +1,10 @@
 import type { App } from "./";
+import { treaty } from "@elysiajs/eden";
 import config from "../../../config.json";
 
 import { edenFetch } from "@elysiajs/eden";
 
-const fetch = edenFetch<App>(`http://${config.FRONTEND_HOST}:${config.FRONTEND_PORT}`);
+export const fetch = edenFetch<App>(`http://${config.FRONTEND_HOST}:${config.FRONTEND_PORT}`);
+export const client = treaty<App>("localhost:3000");
 
 export default fetch;
