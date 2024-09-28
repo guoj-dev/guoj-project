1a0bc83ea1e1f07be94d863861276a06a2c6f6b8
quank123wip <quank123wip@outlook.com>
2024-09-28 09:15:00 +0800

    feat🛠️: organization view prototype

--- apps/web/pages/organization/[name].vue
+++ apps/web/pages/organization/[name].vue
@@ -1,7 +1,14 @@
 <template>
     <div class="container flex flex-col flex-grow h-full">
-        <div>
-            
+        <div class="mt-16 px-8 flex flex-col">
+            <div class="flex">
+                <div class="aspect-square w-32 bg-gray-200 rounded-xl border shadow-lg"></div>
+                <div class="ml-8">
+                    <p class="font-bold text-2xl mb-2">Organization</p>
+                    <p class="font-thin text-xl mb-2">Organization Description</p>
+                </div>
+            </div>
+            <div class="divider mt-4 w-full border"></div>
         </div>
     </div>
 </template>
---
+++ apps/web/pages/problemlist/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container flex flex-col flex-grow h-full">
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
