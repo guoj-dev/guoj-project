3538ef593f378782f9903bc6912dc4f5886f487a
quank123wip <quank123wip@outlook.com>
2024-09-30 23:28:46 +0800

    chore🛠️: init submission view

---
+++ apps/web/pages/submissions/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container flex flex-col flex-grow h-full">
+        <div class="mt-8 px-8 flex flex-col">
+            <div class="flex">
+                <div class="ml-8">
+                    <p class="font-bold text-4xl">Submission #1</p>
+                </div>
+            </div>
+            <div class="divider mt-4 w-full border"></div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
